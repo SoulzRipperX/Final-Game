5daee2f91e67e007d83c843c73c0bf58418ef7da
Soul <wira20121@gmail.com>
2026-03-31 03:24:25 +0700

    Add More Options For Menu

--- Assets/Script/MenuController.cs
+++ Assets/Script/MenuController.cs
@@ -6,16 +6,32 @@ using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
+    private const string BgmVolumeKey = "BGM_VOLUME";
+    private const string SfxVolumeKey = "SFX_VOLUME";
+
     public GameObject optionsMenu;
-    public Slider volumeSlider;
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private Slider sfxSlider;
+    [SerializeField] private AudioSource[] bgmSources;
+    [SerializeField] private AudioSource[] sfxSources;
 
     void Start()
     {
-        if (volumeSlider != null)
+        float bgmVolume = PlayerPrefs.GetFloat(BgmVolumeKey, 1f);
+        float sfxVolume = PlayerPrefs.GetFloat(SfxVolumeKey, 1f);
+
+        if (bgmSlider != null)
         {
-            volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1f);
-            AudioListener.volume = volumeSlider.value;
+            bgmSlider.SetValueWithoutNotify(bgmVolume);
         }
+
+        if (sfxSlider != null)
+        {
+            sfxSlider.SetValueWithoutNotify(sfxVolume);
+        }
+
+        SetBgmVolume(bgmVolume);
+        SetSfxVolume(sfxVolume);
     }
 
     public void PlayGame()
@@ -45,13 +61,35 @@ public class MenuController : MonoBehaviour
         }
     }
 
-    public void AdjustVolume()
+    public void SetBgmVolume(float volume)
     {
-        if (volumeSlider != null)
+        volume = Mathf.Clamp01(volume);
+        PlayerPrefs.SetFloat(BgmVolumeKey, volume);
+        PlayerPrefs.Save();
+        ApplyVolumeToSources(bgmSources, volume);
+    }
+
+    public void SetSfxVolume(float volume)
+    {
+        volume = Mathf.Clamp01(volume);
+        PlayerPrefs.SetFloat(SfxVolumeKey, volume);
+        PlayerPrefs.Save();
+        ApplyVolumeToSources(sfxSources, volume);
+    }
+
+    private static void ApplyVolumeToSources(AudioSource[] sources, float volume)
+    {
+        if (sources == null)
         {
-            AudioListener.volume = volumeSlider.value;
-            PlayerPrefs.SetFloat("Volume", volumeSlider.value);
-            PlayerPrefs.Save();
+            return;
+        }
+
+        foreach (AudioSource source in sources)
+        {
+            if (source != null)
+            {
+                source.volume = volume;
+            }
         }
     }
 }
--- Assets/Script/PasueGameController.cs
+++ Assets/Script/PasueGameController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PasueGameControll : MonoBehaviour
+public class PasueGameController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
